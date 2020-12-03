7c69e04f02c5a9d583121406c8d6a687d98a8ec7
Thomas Leard <44568193+Tleard@users.noreply.github.com>
2020-12-03 10:02:31 +0100

    Added base layout view and theme (#2)
    * Added base layout view and theme
    
    * Navbar clean code

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,9 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use Laravel\Lumen\Http\Request;
 use Laravel\Lumen\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
-    //
+    public function default()
+    {
+        return view('index');
+    }
 }
