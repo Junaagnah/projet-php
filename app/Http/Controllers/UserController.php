ebaf3a15e3e8aefce78ccfcd416039f3cbc4c5b8
Tleard <thomas.leard@gmail.com>
2021-01-03 00:22:13 +0100

    Edit user info + Add firstname, lastname, title and private settings

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,10 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Traits\SessionTrait;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+use Illuminate\Validation\ValidationException;
 use Illuminate\View\View;
 use Laravel\Lumen\Routing\Controller as BaseController;
 use App\User;
+use mysql_xdevapi\Exception;
 
 class UserController extends BaseController {
 
@@ -25,5 +29,74 @@ class UserController extends BaseController {
         else {
             return View('errors', ['error' => 'L\'utilisateur n\'existe pas.']);
         }
+    }
+
+    /**
+     * @param Request $request
+     * @return Exception|ValidationException|View
+     */
+    public function editUserAction(Request $request, string $username)
+    {
+        $input = $request->all();
+
+        $user = User::where('username', $username)->first();
+
+        $password = hash('sha256', $input['password_confirmation']);
+
+        //Checked if user is user or admin
+        //dd($user->getAuthPassword());
+        //if ($user->getAuthPassword() === $hashedPassword)
+
+        dump($input);
+        //if ($SESSION[])
+        $encryptedUsername = $request->cookie(COOKIE_SESSION_KEY);
+        $userLoggedIn = SessionTrait::getSessionCookieValue($encryptedUsername);
+        $currentUser = User::getOneUserByUsername($userLoggedIn);
+
+        dump($currentUser['password']);
+        dump($input['password_confirmation']);
+        dump($password);
+        if ($currentUser['password'] == $password && $currentUser['username'] == $input['username'] || $currentUser['userRole'] == "ROLE_ADMIN")
+        {
+            die();
+        }
+
+        if ($input['password'] !== null) {
+            $password = hash('sha256', $input['password']);
+            $input['password'] = $password;
+        }
+
+        if (isset($input['private']) && $input['private'] !== '')
+        {
+            $input['private'] = intval($input['private']);
+        }
+
+        if (isset($input['profile_picture']) && $input['profile_picture'] !== NULL)
+        {
+            $image_name = Str:: uuid() .'.'. $request->file('profile_picture')->getClientOriginalExtension();
+            $request->file('profile_picture')->move('images/profile_picture', $image_name);
+            $input['profilePicturePath'] = $image_name;
+
+            /*try {
+                $this->validate($request, [
+                    'profile_picture' => 'image|max:32.896',
+
+                ]);
+            } catch (ValidationException $errors) {
+                return view('errors', ['error' => $errors->getResponse()->getContent()]);
+            }*/
+        }
+
+        foreach ($input as $key => $value) {
+            if ($value === '')
+            {
+                unset($input[$key]);
+            }
+        }
+
+        $user->update($input);
+
+        // Once the user is registered, we connect him and redirect to index
+        return View('profile', ['user' => $user]);
     }
 }
