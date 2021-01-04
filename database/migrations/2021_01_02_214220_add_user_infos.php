484b36752d5a1187252128d0246bd62f73d61108
Nicolas G <nicolas.gehringer@outlook.fr>
2021-01-04 13:36:19 +0100

    Added default admin account on migration (#25)
    * Added default admin account on migration
    
    * Fixed User object for Role

--- database/migrations/2021_01_02_214220_add_user_infos.php
+++ database/migrations/2021_01_02_214220_add_user_infos.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use App\User;
 
 class addUserInfos extends Migration
 {
@@ -20,6 +21,20 @@ class addUserInfos extends Migration
             $table->boolean('private')->default(true);
         });
 
+        // Adding admin user
+        $userInput = [
+            'username' => 'Administrator',
+            'email' => 'admin@moviesplaceholder.com',
+            'password' => hash('sha256', 'Password123*'),
+            'userRole' => ROLE_ADMIN,
+            'firstName' => 'Admin',
+            'lastName' => 'Admin',
+            'title' => 'Monsieur',
+            'private' => 1
+        ];
+
+        $user = new User($userInput);
+        $user->save();
     }
 
     /**
