c5b64b04d3178b025f4c3f99e449a3f67dc0d3ce
Thomas Leard <44568193+Tleard@users.noreply.github.com>
2021-01-04 16:52:27 +0100

    Add favico (#48)

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -5,6 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="language" content="fr"/>
     <meta name="Robots" content="index, follow">
+    <link rel="icon" href="{{url("images/logo.png")}}" />
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>MoviesPlaceholder</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
