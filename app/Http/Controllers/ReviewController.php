86b3e44e71cb22c4a2d4411d184f8a53d4a535af
Nicolas G <nicolas.gehringer@outlook.fr>
2021-01-04 16:03:32 +0100

    Fix timezone in .env.example & updated_at review date format (#42)

--- app/Http/Controllers/ReviewController.php
+++ app/Http/Controllers/ReviewController.php
@@ -68,7 +68,7 @@ class ReviewController extends BaseController
         }
 
         // Update the review
-        DB::table('reviews')->where('FK_movieID', $input['FK_movieId'])->where('FK_userId', $_SESSION['user']['id'])->update(['review' => $input['review'], 'note' => $input['note'], 'updated_at' => date('Y-m-d h:i:s')]);
+        DB::table('reviews')->where('FK_movieID', $input['FK_movieId'])->where('FK_userId', $_SESSION['user']['id'])->update(['review' => $input['review'], 'note' => $input['note'], 'updated_at' => date('Y-m-d H:i:s')]);
 
         // Return the movie view
         return redirect('/movieOverview?movieId=' . $input['FK_movieId']);
