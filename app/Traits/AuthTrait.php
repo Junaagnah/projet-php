5db63c82a335d2032073a3f508a37c81579c474f
GEHRINGER Nicolas <nicolas.gehringer@outlook.fr>
2021-01-05 11:33:09 +0100

    Added AuthTrait and moved logic from controller in trait

---
+++ app/Traits/AuthTrait.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Traits;
+
+use App\Http\Controllers\AuthController;
+use App\User;
+use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
+use Illuminate\View\View;
+use Illuminate\Http\RedirectResponse;
+use Laravel\Lumen\Routing\Controller as BaseController;
+
+class AuthTrait {
+
+    /**
+     * @param Request $request
+     * @return Void | View
+     */
+    public static function register(Request $request) {
+        $input = $request->all();
+        $authController = new AuthController();
+
+        $password = hash('sha256', $input['password']);
+
+        try {
+            $authController->validate($request, [
+                'username' => ['required', 'unique:users', 'alpha_num'],
+                'firstName' => ['required'],
+                'lastName' => ['required'],
+                'title' => ['required'],
+                'email' => ['required', 'email', 'unique:users'],
+                'password' => ['required', 'same:password_confirmation']
+            ]);
+        } catch (ValidationException $errors) {
+            return View('errors', ['error' => $errors->getResponse()->getContent()]);
+        }
+
+        //Define password as hashed password
+        $input['password'] = $password;
+
+        $user = New User($input);
+
+        $user->save();
+    }
+
+    /**
+     * @param Request $request
+     * @return RedirectResponse|View
+     */
+    public static function login(Request $request) {
+        // Hashing password
+        $hashedPassword = hash('sha256', $request->get('password'));
+        $authController = new AuthController();
+
+        // Checking if fields are correct
+        try {
+            $authController->validate($request, [
+                'email' => ['required', 'email'],
+                'password' => ['required'],
+            ]);
+        } catch (ValidationException $errors) {
+            return View('errors', ['error' => $errors->getResponse()->getContent()]);
+        }
+
+        // Trying to authenticate user
+        $user = User::getOneUserByEmail($request->get('email'));
+        if (!empty($user) && $user->getAuthPassword() === $hashedPassword) {
+            // Checking if the user is banned
+            if (!$user->getAttribute('isBanned')) {
+                // Setting cookies and redirect to home page
+                SessionTrait::setSessionCookie($user->getAttributeValue('username'));
+                return redirect('/');
+            }
+            else {
+                return View('errors', ['error' => 'Vous avez été banni et ne pouvez plus vous connecter.']);
+            }
+        }
+        else {
+            return View('errors', ['error' => 'Adresse e-mail ou mot de passe incorrect.']);
+        }
+    }
+}
