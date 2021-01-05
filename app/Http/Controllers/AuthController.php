0c1f808ef3f63f312f44ac4bdc3fa063d38320e4
Nicolas G <nicolas.gehringer@outlook.fr>
2021-01-05 11:42:55 +0100

    Added AuthTrait and moved logic from controller in trait (#54)
    * Added AuthTrait and moved logic from controller in trait
    
    * Changed authController to BaseController in AuthTrait
    
    * Changed class to trait

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,13 +3,11 @@
 namespace App\Http\Controllers;
 
 use App\Traits\SessionTrait;
-use Exception;
+use App\Traits\AuthTrait;
 use Illuminate\Http\RedirectResponse;
-use Illuminate\Validation\ValidationException;
 use Illuminate\View\View;
 use Illuminate\Http\Request;
 use Laravel\Lumen\Routing\Controller as BaseController;
-use App\Models\User;
 
 class AuthController extends BaseController
 {
@@ -31,33 +29,15 @@ class AuthController extends BaseController
 
     /**
      * @param Request $request
-     * @return Exception|ValidationException|View
+     * @return View|RedirectResponse
      */
     public function registerAction(Request $request)
     {
-        $input = $request->all();
+        $result = AuthTrait::register($request);
 
-        $password = hash('sha256', $input['password']);
-
-        try {
-            $this->validate($request, [
-                'username' => ['required', 'unique:users', 'alpha_num'],
-                'firstName' => ['required'],
-                'lastName' => ['required'],
-                'title' => ['required'],
-                'email' => ['required', 'email', 'unique:users'],
-                'password' => ['required', 'same:password_confirmation']
-            ]);
-        } catch (ValidationException $errors) {
-            return view('errors', ['error' => $errors->getResponse()->getContent()]);
-        }
-
-        //Define password as hashed password
-        $input['password'] = $password;
-
-        $user = New User($input);
-
-        $user->save();
+        // If result is not empty we might have an error, so we return it
+        if (!empty($result))
+            return $result;
 
         // Once the user is registered, we connect him and redirect to index
         return $this->loginAction($request);
@@ -65,39 +45,11 @@ class AuthController extends BaseController
 
     /**
      * @param Request $request
-     * @return Exception|ValidationException|View|RedirectResponse
+     * @return View|RedirectResponse
      */
     public function loginAction(Request $request)
     {
-        // Hashing password
-        $hashedPassword = hash('sha256', $request->get('password'));
-
-        // Checking if fields are correct
-        try {
-            $this->validate($request, [
-                'email' => ['required', 'email'],
-                'password' => ['required'],
-            ]);
-        } catch (ValidationException $errors) {
-            return view('errors', ['error' => $errors->getResponse()->getContent()]);
-        }
-
-        // Trying to authenticate user
-        $user = User::getOneUserByEmail($request->get('email'));
-        if (!empty($user) && $user->getAuthPassword() === $hashedPassword) {
-            // Checking if the user is banned
-            if (!$user->getAttribute('isBanned')) {
-                // Setting cookies and redirect to home page
-                SessionTrait::setSessionCookie($user->getAttributeValue('username'));
-                return redirect('/');
-            }
-            else {
-                return view('errors', ['error' => 'Vous avez été banni et ne pouvez plus vous connecter.']);
-            }
-        }
-        else {
-            return view('errors', ['error' => 'Adresse e-mail ou mot de passe incorrect.']);
-        }
+        return AuthTrait::login($request);
     }
 
     /**
